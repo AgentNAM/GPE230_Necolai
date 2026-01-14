9453fa7747d01d94d12dc0ea97016d5047d02776
AgentNAM <99711333+AgentNAM@users.noreply.github.com>
2026-01-14 15:29:14 -0700

    Add save system

---
+++ Source/Fall2025CPP/Public/NECOLAI_SaveGame.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "NECOLAI_SaveGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FALL2025CPP_API UNECOLAI_SaveGame : public USaveGame
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(BlueprintReadWrite)
+	int furthestCompletedLevel;
+
+	UPROPERTY(BlueprintReadWrite)
+	float masterVolumeSetting;
+
+	UPROPERTY(BlueprintReadWrite)
+	float musicVolumeSetting;
+
+	UPROPERTY(BlueprintReadWrite)
+	float sfxVolumeSetting;
+};
